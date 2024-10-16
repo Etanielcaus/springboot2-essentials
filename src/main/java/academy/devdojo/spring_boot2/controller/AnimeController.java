3ab14731d4a7bf9ae53af2e496c29201e2735bc9
Etanielcaus <nielfilosofia@gmail.com>
2024-10-15 23:39:55 -0300

    Spring Boot 2 Essentials 16 - Request Params

--- src/main/java/academy/devdojo/spring_boot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/spring_boot2/controller/AnimeController.java
@@ -27,6 +27,16 @@ public class AnimeController {
         return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
     }
 
+    @GetMapping(path = "/find")
+    public ResponseEntity<List<Anime>> findByName(@RequestParam String name){
+        return new ResponseEntity<>(animeService.findByName(name), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/findcontaining")
+    public ResponseEntity<List<Anime>> findByNameContaining(@RequestParam String name){
+        return new ResponseEntity<>(animeService.findByNameContaining(name), HttpStatus.OK);
+    }
+
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
--- src/main/java/academy/devdojo/spring_boot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/spring_boot2/service/AnimeService.java
@@ -23,6 +23,16 @@ public class AnimeService {
         return animeRepository.findAll();
     }
 
+    public List<Anime> findByName(String name){
+        return animeRepository.findByName(name);
+    }
+
+    public List<Anime> findByNameContaining(String name){
+        return animeRepository.findByNameContaining(name);
+    }
+
+
+
     public Anime findByIdOrThrowBadRequest(Long id){
         return animeRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
