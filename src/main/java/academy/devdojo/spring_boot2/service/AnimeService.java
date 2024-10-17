97cea891dcf606566b380784fcf564d0e0fcb552
Etanielcaus <nielfilosofia@gmail.com>
2024-10-16 22:59:22 -0300

    Spring Boot 2 Essentials 19 - Transações

--- src/main/java/academy/devdojo/spring_boot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/spring_boot2/service/AnimeService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -39,6 +40,7 @@ public class AnimeService {
                 .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
     }
